e5d445d2b6f3d9a42a0038a50b8db1b7e8c5ceab
Chase <chasebrown70131@gmail.com>
2026-07-14 21:06:26 -0500

    add command line prompt that inverts current dark mode setting

--- Luna/LunaApp.swift
+++ Luna/LunaApp.swift
@@ -6,6 +6,22 @@
 //
 
 import SwiftUI
+import Foundation   // allows for use of URL & the Process() class to be used to run system level commands
+
+func toggleDarkMode() {
+    let process = Process()
+    
+    // points to the tool that handles and runs scripts
+    process.executableURL = URL(fileURLWithPath: "/usr/bin/osascript")
+   
+    // tells macOS to invert the current DarkMode state
+    process.arguments = ["-e", "tell application \"System Events\" to tell appearance preferences to set dark mode to not dark mode"]
+    do {
+        try process.run()
+    } catch {
+        print("Failed to toggle Dark Mode: /(error)")
+    }
+}
 
 @main
 struct LunaApp: App {
