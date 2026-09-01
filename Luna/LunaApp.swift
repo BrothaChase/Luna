c8c9d5c9fdc74bd335a7bfce6347eee327e4b59b
Chase <chasebrown70131@gmail.com>
2026-08-31 23:45:39 -0500

    Initial Commit

---
+++ Luna/LunaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunaApp.swift
+//  Luna
+//
+//  Created by Chase brown on 8/31/26.
+//
+
+import SwiftUI
+
+@main
+struct LunaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
